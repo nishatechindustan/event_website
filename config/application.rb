bfd2fc32edac7b06ed7161653015a307b62badf6
Ashish <ashish.techindustan@gmail.com>
2018-01-09 14:08:14 +0530

    change database

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module Myappfgfg
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     #config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += ["#{Rails.root.to_s}/lib"] 
+    #config.autoload_paths += ["#{Rails.root.to_s}/lib"] 
   end
 end
