f63f28b1f12021c50508493799070f4472a5dbb4
Ashish <ashish.techindustan@gmail.com>
2018-01-09 13:18:31 +0530

    change production file

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Myappfgfg
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.active_record.raise_in_transactional_callbacks = true
-    # config.autoload_paths += ["#{Rails.root.to_s}/lib"] 
+    config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += ["#{Rails.root.to_s}/lib"] 
   end
 end
