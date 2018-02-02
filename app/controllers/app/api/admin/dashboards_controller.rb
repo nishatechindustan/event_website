6c41b85574a40da842f49282e41d3d1d90eb96b4
Ashish <ashish.techindustan@gmail.com>
2018-02-02 19:42:14 +0530

    update users list details

--- app/controllers/app/api/admin/dashboards_controller.rb
+++ app/controllers/app/api/admin/dashboards_controller.rb
@@ -12,9 +12,8 @@ class App::Api::Admin::DashboardsController < AdminController
     users= []
     @users.each do |user|
       @user_image =  user.attachments.present? ? user.attachments.first.attachment.url : '';
-      users<<{:auth_token=>user.auth_token, :id=>user.id :email=>user.email, :user_name => user.user_name, :first_name=> user.first_name, :last_name=> user.last_name, :is_admin => user.is_admin, :image=> @user_image}
+      users<<{:auth_token=>user.auth_token, :id=>user.id, :email=>user.email, :user_name => user.user_name, :first_name=> user.first_name, :last_name=> user.last_name, :is_admin => user.is_admin, :image=> @user_image}
     end
-
     render :json =>{result: users, status: 200}
   end
 
