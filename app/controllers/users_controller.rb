44c01efc5dc29acee0043c88c6d9511c816c7e41
Ashish <ashish.techindustan@gmail.com>
2018-02-02 18:45:37 +0530

    update image params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,28 +13,26 @@ class UsersController < ApplicationController
 
 	#update user
 	def update
-		 @user= User.find_by(auth_token: params[:auth_token])
-		 if @user.present?
-			 if @user.update(users_params)
-			# 	if params[:user][:avatar_image].present? && avatar_image_params.present?#avatar_image_params[:attachment].present?
-			# 		@user_image = 	@user.attachments.where(:attachable_id => @user.id, :attachable_type => "User")
-			# 		if @user_image.present?
-			# 			@user.attachments.update(avatar_image_params)
-			# 		else
-			# 		@user.attachments.create(avatar_image_params)
-			# 		end
-			# 	end
-			# 	flash[:notice] =  "Profile updated successfully"
-			# 	redirect_to users_setting_path
+		@user= User.find_by(auth_token: params[:auth_token])
+		if @user.present?
+			if @user.update(users_params)
+				if params[:user][:attachment].present? && avatar_image_params.present?
+					@user_image = @user.attachments.where(:attachable_id => @user.id, :attachable_type => "User")
+					if @user_image.present?
+						@user.attachments.update(avatar_image_params)
+					else
+					@user.attachments.create(avatar_image_params)
+					end
+				end
 			@user_image =  @user.attachments.present? ? @user.attachments.first.attachment.url : '';
 			userDetails = {:auth_token=>@user.auth_token, :email=>@user.email, :user_name => @user.user_name, :first_name=> @user.first_name, :last_name=> @user.last_name, :is_admin => @user.is_admin, :image=> @user_image}
-					render :json=> {:status=> true, :message=>" Profile updated successfully", :userDetails=>userDetails}
-			 	else
-				 	render :json=> {:status=> true, :message=>@user.errors.full_messages}
-				end
-			else
-				render :json=> {:status=> false, :message=>"Invalid token"}
+			render :json=> {:status=> true, :message=>" Profile updated successfully", :userDetails=>userDetails}
+		 	else
+			 	render :json=> {:status=> true, :message=>@user.errors.full_messages}
 			end
+		else
+			render :json=> {:status=> false, :message=>"Invalid token"}
+		end
 	end
 
 	def destroy
@@ -51,7 +49,7 @@ class UsersController < ApplicationController
 	# for photo params
 
 	def avatar_image_params
-		params.require(:user).require(:avatar_image).permit(:attachment)
+		params.require(:user).require(:attachment).permit(:attachment)
 	end
 
 
