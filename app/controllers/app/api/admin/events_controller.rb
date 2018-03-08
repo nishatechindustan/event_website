68531dc69cb266f5edf34754859fd6bc50c9cbc8
Ashish <ashish.techindustan@gmail.com>
2018-03-08 15:59:06 +0530

    changes in update event apis

--- app/controllers/app/api/admin/events_controller.rb
+++ app/controllers/app/api/admin/events_controller.rb
@@ -55,8 +55,8 @@ class App::Api::Admin::EventsController < AdminController
   end
 
   def update
-      @event.category_ids = params[:category_ids].values
-      @event.artist_ids = params[:artist_ids].values
+      @event.category_ids = params[:category_ids]
+      @event.artist_ids = params[:artist_ids]
       @event.event_location = event_location
       @event.event_dates = event_dates
         if @event.update(event_params)
