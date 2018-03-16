61dca45f8749aeb148fbf3ea4e2191a258c127f9
Ashish <ashish.techindustan@gmail.com>
2018-03-16 15:30:27 +0530

    modified image folders

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,10 +3,10 @@ class Attachment < ApplicationRecord
 	#attr_accessor :image_base
 	#before_validation :parse_image
 
-	has_attached_file :attachment, path: '#{Rails.root}/:class/:attachment/:id_partition/:style/:filename',
+	has_attached_file :attachment, path: ':rails_root/public/assets/:class/:attachment/:id_partition/:style/:filename',
  		styles: {thumb: "100x100"}
-	validates_attachment_content_type :attachment, content_type: ["image/jpeg", "image/jpg", "image/png", "image/gif","application/pdf"]
-	":rails_root/public/system/:rails_env/:class/:attachment/:id_partition/:filename"
+	validates_attachment_content_type :attachment, content_type: ["image/jpeg", "image/jpg", "image/png", "image/gif"]
+	# ":rails_root/public/system/:rails_env/:class/:attachment/:id_partition/:filename"
 	# private
 	#  def parse_image
 	# 	 image = Paperclip.io_adapters.for(image_base) image.original_filename = "file.jpg"
