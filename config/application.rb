bdb3aaffe00293c28b4f54d99c5a5768cc823386
Ashish <ashish.techindustan@gmail.com>
2018-03-16 13:15:19 +0530

    assetes clean

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Myappfgfg
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.enabled = false
     config.load_defaults 5.1
     config.action_dispatch.default_headers = {
       "Access-Control-Allow-Origin" => "*",
