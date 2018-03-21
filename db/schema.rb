c5e2ba6cdc1fad9dfeb7cd06a34534b1b8e58f63
Ashish <ashish.techindustan@gmail.com>
2018-03-21 11:30:10 +0530

    add field to location tabbe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228061811) do
+ActiveRecord::Schema.define(version: 20180321054550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,9 @@ ActiveRecord::Schema.define(version: 20180228061811) do
     t.string "venue"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "state"
+    t.string "country"
   end
 
   create_table "users", force: :cascade do |t|
