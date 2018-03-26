4b90907b7d45fade09a0ba9e541eab1225693054
Ashish <ashish.techindustan@gmail.com>
2018-03-26 19:26:43 +0530

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,47 @@
 class ApplicationController < ActionController::API
-   before_action :check_email, if: :user_signed_in?
-   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def check_email
-    if !current_user.is_admin? && current_user.uid.present?
-      if params[:controller]!="users"
-        if params[:controller]=="devise/sessions"
-          return
-        end
+	include Response
+  	include ExceptionHandler
 
-        if current_user.email.include?(current_user.uid)
-           flash[:notice]="Please update email address"
-           redirect_to users_setting_path and return
-      	elsif params[:action] != "edit" && params[:action] != "update"
-	        if current_user.email.include?("facebook") || current_user.email.include?("twitter") || current_user.email.include?("linkedin")
-            flash[:notice]="Please update email address"
-            redirect_to users_setting_path and return
-	        end
-        end
-      end
-    end
-  end
+   	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:account_update, keys: [:user_name,:first_name,:last_name,:location,:latitude,:longitude,:password, :password_confirmation, :current_password, :image ])
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:first_name,:last_name,:latitude,:longitude,:password, :location, :password_confirmation, :current_password, :image ])
+	   devise_parameter_sanitizer.permit(:account_update, keys: [:user_name,:first_name,:last_name,:location,:latitude,:longitude,:password, :password_confirmation, :current_password, :image ])
+	   devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:first_name,:last_name,:latitude,:longitude,:password, :location, :password_confirmation, :current_password, :image ])
 	end
 end
+# class ApplicationController < ActionController::API
+#   attr_reader :current_user
+#   before_action :configure_permitted_parameters, if: :devise_controller?
+
+#     protected
+#     def authenticate_request!
+#       unless user_id_in_token?
+#         render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+#         return
+#       end
+#       @current_user = User.find(auth_token[:user_id])
+#       rescue JWT::VerificationError, JWT::DecodeError
+#         render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+#     end
+
+#     private
+#     def http_token
+#         @http_token ||= if request.headers['Authorization'].present?
+#           request.headers['Authorization'].split(' ').last
+#         end
+#     end
+
+#     def auth_token
+#       @auth_token ||= JsonWebToken.decode(http_token)
+#     end
+
+#     def user_id_in_token?
+#       http_token && auth_token && auth_token[:user_id].to_i
+#     end
+
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name,:first_name,:last_name,:location,:latitude,:longitude,:password, :password_confirmation, :current_password, :image ])
+#     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:first_name,:last_name,:latitude,:longitude,:password, :location, :password_confirmation, :current_password, :image ])
+#   end
+# end
