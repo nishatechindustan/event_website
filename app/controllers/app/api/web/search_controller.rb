cb413796db5dbaefe252a58621abae7df7d08fef
ashish <ashish.techindustan@gmail.com>
2018-03-27 19:32:52 +0530

    event search  apis

--- app/controllers/app/api/web/search_controller.rb
+++ app/controllers/app/api/web/search_controller.rb
@@ -3,7 +3,7 @@ class App::Api::Web::SearchController < ApplicationController
 	def search
 		events = Event.search(params)
 		# @events = {:status=>true, :}
-		response = {:status=>true, :events=>events}
+		response = {:status=>true, :total_event=>events.count, :events=>events }
 		json_response(response)
 	end
 end
