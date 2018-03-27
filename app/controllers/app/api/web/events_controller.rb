6d804edc2fd556c66d20ab7fa3b192a2860b01ad
ashish <ashish.techindustan@gmail.com>
2018-03-27 17:33:26 +0530

    event show apis

--- app/controllers/app/api/web/events_controller.rb
+++ app/controllers/app/api/web/events_controller.rb
@@ -31,11 +31,16 @@ class App::Api::Web::EventsController < ApplicationController
 		@event_image = @event.attachments.present? ? @event.attachments.first.attachment.url : '/default_image.jpg';
 		@event_currency = @event.currency.present? ? @event.currency: ''
 		event_type = @event.event_type=="0" ? "free" : "paid"
+		artists =[]
+		@event_artists.each do |a|
+			artists<< {:id=> a.id,:name=>a.name ,:address=>a.address,:description=> a.description, :type=> a.artist_type, :image=> a.attachments.present? ? @event.attachments.first.attachment.url : '/default_image.jpg'
+		end
  		event_date_time = {:start_date=>@event_dates.start_date,:end_date=>@event_dates.end_date ,:start_time=>@event_dates.start_time,:end_time=>@event_dates.end_time}
 		event_location= {:address=>@event_location.address,:latitude=>@event_location.latitude,:longitude=>@event_location.longitude,:venue=>@event_location.venue,:locality=> @event_location.city,:administrative_area_level_1=>@event_location.state,:country=>@event_location.country}
 
 		event = {:id=>@event.id,:title=>@event.title, :description=> @event.description, :ticket_available=>@event.ticket_available,:cost=>@event.cost, :currency=>@event_currency, :contact_number=>@event.contact_number,:cost_offers=>@event.cost_offers, :email=>@event.email, :event_type=>event_type, :status=>@event.status, :event_categories=>@event_categories.map(&:name), :event_location=>event_location,
-		  :event_artists=>@event_artists.map(&:name),:event_date=>event_date_time, :image=>@event_image}
+		  :event_artists=>artists}
+,:event_date=>event_date_time, :image=>@event_image}
 		 response ={:status=>true, :event=>event}
 		    
 		json_response(response)
