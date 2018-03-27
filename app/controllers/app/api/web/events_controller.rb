2d2e82e257ea41975762bd8641861bfe94746075
ashish <ashish.techindustan@gmail.com>
2018-03-27 15:50:35 +0530

    create new apis for paid, free for event

--- app/controllers/app/api/web/events_controller.rb
+++ app/controllers/app/api/web/events_controller.rb
@@ -1,18 +1,18 @@
 class App::Api::Web::EventsController < ApplicationController
 	def today_event
 		events = Event.fetch_event("today")	
-		response = {:status=>true,:events=>events}
+		response = {:status=>true,:events=>events, :total_events=> events.count}
 		json_response(response)
 	end
 	def paid_event
 		events = Event.fetch_event("paid")	
-		response = {:status=>true,:events=>events}
+		response = {:status=>true,:events=>events, :total_events=> events.count}
 		json_response(response)
 	end
 
 	def free_event
 		events = Event.fetch_event("free")	
-		response = {:status=>true,:events=>events}
+		response = {:status=>true,:events=>events, :total_events=> events.count}
 		json_response(response)
 	end
 end
