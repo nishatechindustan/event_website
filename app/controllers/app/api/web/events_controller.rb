0cb6b3677386f8c027d940b38beb91a8d6297c15
ashish <ashish.techindustan@gmail.com>
2018-03-27 17:11:10 +0530

    event show apis

--- app/controllers/app/api/web/events_controller.rb
+++ app/controllers/app/api/web/events_controller.rb
@@ -25,15 +25,17 @@ class App::Api::Web::EventsController < ApplicationController
 		response = {:status=>true,:events=>events, :total_events=> events.count}
 		json_response(response)
 	end
+	
 	def event_details
 
 		@event_image = @event.attachments.present? ? @event.attachments.first.attachment.url : '/default_image.jpg';
 		@event_currency = @event.currency.present? ? @event.currency: ''
-		event_date_time = {:start_date=>@event_dates.start_date,:end_date=>@event_dates.end_date ,:start_time=>@event_dates.start_time,:end_time=>@event_dates.end_time}
+		event_type = @event.event_type=="0" ? "free" : "paid"
+ 		event_date_time = {:start_date=>@event_dates.start_date,:end_date=>@event_dates.end_date ,:start_time=>@event_dates.start_time,:end_time=>@event_dates.end_time}
 		event_location= {:address=>@event_location.address,:latitude=>@event_location.latitude,:longitude=>@event_location.longitude,:venue=>@event_location.venue,:locality=> @event_location.city,:administrative_area_level_1=>@event_location.state,:country=>@event_location.country}
 
-		event = {:event_categories=>@event_categories.map(&:id), :event_location=>event_location,
-		  :event_artists=>@event_artists.map(&:id),:event_date=>event_date_time,:event=>@event, :event_image=>@event_image}
+		event = {:id=>@event.id,:title=>@event.title, :description=> @event.description, :ticket_available=>@event.ticket_available,:cost=>@event.cost, :currency=>@event_currency, :contact_number=>contact_number,:cost_offers=>@event.cost_offers, :email=>@event.email, :event_type=>event_type, :status=>@event.status, :event_categories=>@event_categories.map(&:name), :event_location=>event_location,
+		  :event_artists=>@event_artists.map(&:name),:event_date=>event_date_time, :image=>@event_image}
 		 response ={:status=>true, :event=>event}
 		    
 		json_response(response)
