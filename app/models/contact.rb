d9bed31b299aab40237a1b42edce6f5164d4f22f
ashish <ashish.techindustan@gmail.com>
2018-03-29 15:05:39 +0530

    create contact us model and table

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
     validates :first_name, :last_name, :email,  presence: true
-    validates_uniqueness_of :email, message: 'Your Request has been  already submitted'
+    validates_uniqueness_of :email
     validates_format_of  :email, :with  => Devise.email_regexp
 end
