fb28fefb1a980ac30c9dac09cc770bee6e255780
ashish <ashish.techindustan@gmail.com>
2018-03-29 15:59:09 +0530

    create advertise  model and table

--- app/models/advertise.rb
+++ app/models/advertise.rb
@@ -1,6 +1,6 @@
 class Advertise < ApplicationRecord
     attr_accessor :advertise_date
-    
+    has_many :event_adver_dates , as: :event_adver_datable, :dependent => :destroy
     validates :organization_name, :contact_person, :contact_number, :event_type, presence: true
     validates :contact_number, :numericality => {:only_integer => true}
     after_save :add_advertise_dates
