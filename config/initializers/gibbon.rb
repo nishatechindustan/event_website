7ff86ea46db082d9a9df1701facb3a0d90984274
ashish <ashish.techindustan@gmail.com>
2018-04-06 18:04:33 +0530

    add mailchip for new user

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,5 +1,4 @@
  # Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"]
- debugger
 Gibbon::Request.api_key = ENV["MAILCHIMP_API_KEY"]
 Gibbon::Request.timeout = 15
 Gibbon::Request.throws_exceptions = true
