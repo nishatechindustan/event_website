434df1e90bc964ffbedfc725ecf84d23b2a4a812
ashish <ashish.techindustan@gmail.com>
2018-04-06 11:45:00 +0530

    change users controller edit method

--- app/controllers/app/api/admin/users_controller.rb
+++ app/controllers/app/api/admin/users_controller.rb
@@ -1,8 +1,8 @@
 class App::Api::Admin::UsersController < AdminController
 	# callbacks
 
-	before_action :authenticate_request!, :except => [:edit, :delete_user,:change_status]
-	before_action :get_user, only: [:delete_user, :edit, :change_status]
+	before_action :authenticate_request!, :except => [:delete_user,:change_status]
+	before_action :get_user, only: [:delete_user,:change_status]
 
 	def all_users
     @users = User.all - [@current_user]
@@ -15,8 +15,9 @@ class App::Api::Admin::UsersController < AdminController
 	end
 
 	def edit
+		@user|| = @current_user
 		@user_image =  @user.attachments.present? ? @user.attachments.first.attachment.url : '/default_image.jpg';
-		user = {:auth_token=>@user.auth_token, :id=>@user.id, :email=>@user.email, :user_name => @user.user_name, :first_name=> @user.first_name, :last_name=> @user.last_name, :is_admin => @user.is_admin, :image=> @user_image}
+		user = {:id=>@user.id, :email=>@user.email, :user_name => @user.user_name, :first_name=> @user.first_name, :last_name=> @user.last_name, :is_admin => @user.is_admin, :image=> @user_image}
 		render :json =>{data: user, status: true}
 	end
 
