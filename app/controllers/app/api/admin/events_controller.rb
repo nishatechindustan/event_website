4b5c7a180e8e1bf6f02747d857b2ccf45c888972
ashish <ashish.techindustan@gmail.com>
2018-04-06 15:19:18 +0530

    event controller create method

--- app/controllers/app/api/admin/events_controller.rb
+++ app/controllers/app/api/admin/events_controller.rb
@@ -20,8 +20,8 @@ class App::Api::Admin::EventsController < AdminController
     user||= @current_user
     if user.present?
       @event = user.events.new(event_params)
-      # @event.category_ids = params[:category_ids]
-      # @event.artist_ids = params[:artist_ids]
+      @event.category_ids = params[:category_ids]
+      @event.artist_ids = params[:artist_ids]
       @event.event_location = event_location
       @event.event_dates = event_dates
       if @event.save
