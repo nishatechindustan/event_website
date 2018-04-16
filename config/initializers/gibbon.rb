cd6fe5ec3d52a38794387749f13d8f079f25f12f
ashish <ashish.techindustan@gmail.com>
2018-04-16 16:02:32 +0530

    change in .env file and added strees testing using jmeter

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,5 +1,5 @@
  # Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"]
-Gibbon::Request.api_key = ENV["MAILCHIMP_API_KEY"]
+Gibbon::Request.api_key = "4c6a3a37873b911b938cebbbc8b308fb-us12"
 Gibbon::Request.timeout = 15
 Gibbon::Request.throws_exceptions = true
 #puts "MailChimp API key: #{Gibbon::Request.api_key}" # temporary
--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 
 RSpec.describe EventMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it "EventMailer" do
+ end
 end
