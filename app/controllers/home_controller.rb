493e79da7fd046212fd50ac049a311bd2bd053d7
ashish <ashish.techindustan@gmail.com>
2018-04-24 11:57:29 +0530

    changes in home conteroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,16 @@
 class HomeController < ApplicationController
-
-  def index
-  	# @users= User.all
-  	# render :json =>{:status=>true,:message=> "List of all users details",:data=> @users}
-  	response = RestClient::Request.execute(
-		method: :get,
-		url: 'https://eventwesite.herokuapp.com/api/v1/web/recent_event')
-		result = JSON.parse(response.body)
-		@recent_event = result["events"]
-  end
+	#before_action :authenticate_user!
+	def index
+		@events = Event.fetch_event('')
+		Rails.logger.info("PARAMS: #{@events}")
+		Rails.logger.info("-------------------------------------------------------")
+		Rails.logger.info(@events)
+		# @users= User.all
+		# render :json =>{:status=>true,:message=> "List of all users details",:data=> @users}
+		# response = RestClient::Request.execute(
+		# method: :get,
+		# url: 'https://eventwesite.herokuapp.com/api/v1/web/recent_event')
+		# result = JSON.parse(response.body)
+		# @recent_event = result["events"]
+	end
 end
