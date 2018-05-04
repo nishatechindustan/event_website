a39acb216fef072aacdcf54a6382680ec41f2c3b
ashish <ashish.techindustan@gmail.com>
2018-05-04 13:08:46 +0530

    uncomment in callback methods

--- app/controllers/api/v1/admin/events_controller.rb
+++ app/controllers/api/v1/admin/events_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1::Admin
   class EventsController < ApiController
     # callbacks
-   # before_action :authenticate_request! , only:[:create,:get_event_list,:latest_event,:event_list]
+    before_action :authenticate_request! , only:[:create,:get_event_list,:latest_event,:event_list]
     before_action :get_category_and_artist ,only:[:create,:new, :edit, :update, :show]
     before_action :get_event_id ,only:[:edit, :destroy, :update ,:show, :change_status,:event_approve_unapprove]
     before_action :get_event_data ,only:[:edit, :update, :show]
